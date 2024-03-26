3f66b6b5e0d0638d29797067e35ffc6266a5bb07
Joseph Alemu <114625022+JosephSAlemu@users.noreply.github.com>
2024-03-25 19:48:31 -0500

    Added WantSpins python file
    Script for stopping spins at things you want

---
+++ main/WantSpins.py
@@ -0,0 +1,50 @@
+import pyautogui
+import os
+import time
+from PIL import Image 
+import pydirectinput
+
+def load_images_from_folder():
+    folder = '.\WantSpin'
+    images = []
+    # Supported image extensions
+    valid_extensions = [".jpg", ".jpeg", ".png", ".gif", ".bmp"]
+    #Just getting every image from the File Path I specify
+    for filename in os.listdir(folder):
+        if any(filename.lower().endswith(ext) for ext in valid_extensions):
+            img_path = os.path.join(folder, filename)
+            images.append(img_path)
+    return images
+
+def wantCheck(images):
+    common = True
+    for image in images:
+        print(image)
+        try:
+            #Mess with the confidence on this if you want to change how accurate the script is detecting images (Do this if you're having detection issues).
+            pyautogui.locateOnScreen(image, confidence=0.8)
+            common = False
+            #Just logging any matches that happen
+            print('Match at' + image)
+        except:
+            pass
+    return common
+
+def Spin():
+    #Code finds the spin button png and clicks it on full screen
+    loc = pyautogui.locateOnScreen('[Put Spin Button file path here]', confidence=0.9)
+    test = pyautogui.center(loc)
+    images = load_images_from_folder()
+    check = True
+    while check:
+        #Positions mouse to spin button
+        pydirectinput.moveTo(test.x, test.y)
+        #Cause roblox doesn't like when you instantly teleport your cursor, this is here to have roblox re-register your mouse so you can auto click
+        pydirectinput.moveTo(test.x, test.y+1)
+        pydirectinput.leftClick()
+        #Sleep cause without it, it can't stop before recognizing images.
+        time.sleep(.25)
+        check = wantCheck(images)
+
+Spin()   
+
